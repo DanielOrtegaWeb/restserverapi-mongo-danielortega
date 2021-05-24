ffc278d0e265b71022deb46e633129939dc6a5cd
daniel ortega <danielfortega92@gmail.com>
2021-05-23 23:09:19 -0400

    restapi cloud

--- database/config.js
+++ database/config.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const ConexionDB= async () => {
     try {
         //await para que espere la conexion
-        await mongoose.connect(process.env.MONGO_CN, {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
